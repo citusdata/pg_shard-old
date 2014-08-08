f4a50cb0e9820eac5ad1ac87e65790763e32f734
Jason Petersen <jason@citusdata.com>
2014-08-08 10:19:53 -0600

    Move typedefs and defines to header
    Sigh.

--- connection.h
+++ connection.h
@@ -20,6 +20,37 @@
 #include "libpq-fe.h"
 
 
+/* Maximum duration to wait for connection */
+#define CLIENT_CONNECT_TIMEOUT_SECONDS "5"
+
+/* Maximum (textual) lengths of hostname */
+#define MAX_NODE_LENGTH 255
+
+/* Times to attempt connection (or reconnection) */
+#define MAX_CONNECT_ATTEMPTS 2
+
+/* SQL statement for testing */
+#define TEST_SQL "DO $$ BEGIN RAISE %s 'Raised remotely!'; END $$"
+
+/*
+ * NodeConnectionKey acts as the key to index into the (process-local) hash
+ * keeping track of open connections. Node name and port are sufficient.
+ */
+typedef struct NodeConnectionKey
+{
+	char nodeName[MAX_NODE_LENGTH + 1];	/* hostname of host to connect to */
+	int32 nodePort;						/* port of host to connect to */
+} NodeConnectionKey;
+
+
+/* NodeConnectionEntry keeps track of connections themselves. */
+typedef struct NodeConnectionEntry
+{
+	NodeConnectionKey cacheKey;	/* hash entry key */
+	PGconn *connection;			/* connection to remote server, if any */
+} NodeConnectionEntry;
+
+
 extern PGconn * GetConnection(char *nodeName, int32 nodePort);
 extern void PurgeConnection(PGconn *connection);
 extern void ReportRemoteSqlError(int errorLevel, PGresult *result, PGconn *connection,
