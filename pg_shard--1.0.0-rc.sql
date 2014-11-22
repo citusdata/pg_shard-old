e05194af4f3d971f2d0c4e3511649e77cacd07e1
Jason Petersen <jason@citusdata.com>
2014-11-21 17:01:21 -0800

    Change version number to 1.0.0-rc
    Our release candidate.

--- pg_shard--1.0.0-rc.sql
+++ pg_shard--1.0.0-rc.sql
@@ -1,4 +1,4 @@
-/* pg_shard--develop.sql */
+/* pg_shard--1.0.0-rc.sql */
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pg_shard" to load this file. \quit
